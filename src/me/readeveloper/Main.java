9ef24a938467b8e3db3a41a3af4f9ecc3ee04f11
Oziel Martinez <ozielrea97@hotmail.com>
2018-05-19 19:32:34 -0500

    Complete calcular method

--- src/me/readeveloper/Main.java
+++ src/me/readeveloper/Main.java
@@ -5,14 +5,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         int cantidad;
+        double alfa;
         double[] numerosAleatorios;
         PruebaPromedio pruebaPromedio;
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Ingresa cantidad de numeros aleatorios a generar: ");
         cantidad = sc.nextInt();
+        System.out.println("Ingresa porcentaje de alfa: ");
+        alfa = sc.nextDouble();
+
         numerosAleatorios = new RandomNumberGenerator(cantidad, 5).make();
-        pruebaPromedio = new PruebaPromedio(numerosAleatorios);
+        pruebaPromedio = new PruebaPromedio(numerosAleatorios, alfa);
         pruebaPromedio.resolver();
     }
 }
--- src/me/readeveloper/PruebaPromedio.java
+++ src/me/readeveloper/PruebaPromedio.java
@@ -1,10 +1,14 @@
 package me.readeveloper;
 
+import java.util.Scanner;
+
 public class PruebaPromedio {
     private double[] numerosAleatorios;
+    private double alfa;
 
-    public PruebaPromedio(double[] numerosAleatorios) {
+    public PruebaPromedio(double[] numerosAleatorios, double alfa) {
         this.numerosAleatorios = numerosAleatorios;
+        this.alfa = alfa;
     }
 
     private double calcularPromedio() {
@@ -17,7 +21,37 @@ public class PruebaPromedio {
         return suma / this.numerosAleatorios.length;
     }
 
+    private double calcularEstadistico(double promedio) {
+        return Math.abs(
+                ((promedio - 0.5) * Math.sqrt(this.numerosAleatorios.length)) / Math.sqrt((1.0 / 12.0))
+        );
+    }
+
+    private double getEstadisticoTablas() {
+        double alfaReal = (100 - this.alfa) / 100;
+        double zABuscar = alfaReal / 2;
+        System.out.println(String.format("Ingresa estadistico de tablas Z %.5f/2", zABuscar));
+        return new Scanner(System.in).nextDouble();
+    }
+
     public void resolver() {
         double promedio = this.calcularPromedio();
+        double estadistico = this.calcularEstadistico(promedio);
+        double estadisticoTablas = this.getEstadisticoTablas();
+
+        System.out.println(estadistico);
+        System.out.println(estadisticoTablas);
+
+        if (estadistico < estadisticoTablas) {
+            System.out.println(
+                String.format("%.5f < %.5f | verdadero", estadistico, estadisticoTablas)
+            );
+            System.out.println("Los numeros son aceptados.");
+        } else {
+            System.out.println(
+                String.format("%.5f < %.5f | falso", estadistico, estadisticoTablas)
+            );
+            System.out.println("Los numeros son rechazados.");
+        }
     }
 }
